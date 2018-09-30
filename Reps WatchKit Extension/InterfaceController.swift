a771ae364a3580c47bf13f0bb7c0f17392ad29e7
Peter Olsen <pto@me.com>
2018-09-30 13:58:25 -0400

    First version

--- Reps WatchKit Extension/InterfaceController.swift
+++ Reps WatchKit Extension/InterfaceController.swift
@@ -11,7 +11,8 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
-
+    @IBOutlet weak var plan: WKInterfaceLabel!
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
@@ -21,6 +22,29 @@ class InterfaceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        let df = DateFormatter()
+        df.dateStyle = .short
+        df.timeZone = .none
+        guard let epic = df.date(from: "10/1/2018") else {
+            plan.setText("Bad Date")
+            return
+        }
+        let today = Date()
+        let seconds = today.timeIntervalSince(epic)
+        guard seconds > 0 else {
+            plan.setText("Wait")
+            return
+        }
+        let day = Int(seconds / 60 / 60 / 24) + 1
+        if day % 2 == 1 {
+            var reps = (day + 1) * 2
+            reps = (reps < 128) ? reps : 128
+            plan.setText("\(reps) reps")
+        } else {
+            var flights = day
+            flights = (flights < 64) ? flights : 64
+            plan.setText("\(flights) flights")
+        }
     }
     
     override func didDeactivate() {
--- Reps/ViewController.swift
+++ Reps/ViewController.swift
@@ -9,10 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var day: UILabel!
+    @IBOutlet weak var plan: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        date.text = " "
+        day.text = " "
+        plan.text = " "
+        let df = DateFormatter()
+        df.dateStyle = .short
+        df.timeZone = .none
+        guard let epic = df.date(from: "10/1/2018") else {
+            date.text = "Bad date"
+            return
+        }
+        let today = Date()
+        date.text = df.string(from: today)
+        let seconds = today.timeIntervalSince(epic)
+        guard seconds > 0 else {
+            plan.text = "Wait"
+            return
+        }
+        let dayNum = Int(seconds / 60 / 60 / 24) + 1
+        day.text = String(dayNum)
+        if dayNum % 2 == 1 {
+            var reps = (dayNum + 1) * 2
+            reps = (reps < 128) ? reps : 128
+            plan.text = "\(reps) reps"
+        } else {
+            var flights = dayNum
+            flights = (flights < 64) ? flights : 64
+            plan.text = "\(flights) flights"
+        }
     }
 
 
